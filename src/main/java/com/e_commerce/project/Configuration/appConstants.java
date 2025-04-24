488c95172889c968f14656e8091bf0fece36c1db
Kavya1633 <kavyasingla30@gmail.com>
2025-04-24 18:53:45 +0530

    App configurations added

---
+++ src/main/java/com/e_commerce/project/Configuration/appConstants.java
@@ -0,0 +1,11 @@
+package com.e_commerce.project.Configuration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class appConstants {
+    public static final String Page_Number="0";
+    public static final String Page_Size="10";
+    public static final String Sort_Categories_By="categoryId";
+    public static final String Sort_Products_By="productId";
+    public static final String Sort_dir="asc";
+}
